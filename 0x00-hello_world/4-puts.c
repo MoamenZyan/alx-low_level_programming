316398e3dad29d67e582a72af2e1351049730654
MoamenZyan <moamenzyan20@gmail.com>
2023-07-13 18:48:21 +0300

    first program!

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+/*it prints out a string*/
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
