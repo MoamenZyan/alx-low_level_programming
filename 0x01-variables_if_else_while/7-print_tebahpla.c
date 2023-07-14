27258a627a369d4dec9c70245b40090ea6637b8f
MoamenZyan <moamenzyan20@gmail.com>
2023-07-14 20:14:39 +0300

    print all alphabet in reverse

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
-
-int main(void) {
+/**
+* main - this is the main function
+* Return: is always 0
+*/
+int main(void)
+{
 	int i;
 
-    	for (i = 122; i >= 97;i--)
+	for (i = 122; i >= 97; i--)
 	{
 		putchar((char)i);
 	}
 
-    putchar('\n');
-    return 0;
+putchar('\n');
+return (0);
 }
