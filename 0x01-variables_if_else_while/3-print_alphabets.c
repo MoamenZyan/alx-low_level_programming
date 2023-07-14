d1fe9314b22a833d115429ef2935629d5038ff8a
MoamenZyan <moamenzyan20@gmail.com>
2023-07-14 19:54:19 +0300

    print lower and upper alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+* main - this is the main function
+* Return: is always 0
+*/
+
+int main(void)
+{
+
+int i;
+
+for (i = 97; i <= 122; i++)
+{
+	putchar((char)i);
+}
+
+putchar('\n');
+
+for (i = 97; i <= 123; i++)
+{
+	if (i == 123)
+	{
+		i = 10;
+		putchar(toupper((char)i));
+		break;
+	}
+	putchar(toupper((char)i));
+}
+
+return (0);
+}
