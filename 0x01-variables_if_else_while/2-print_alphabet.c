dda35258ecd53848e890ce3fd563976dee3205fd
MoamenZyan <moamenzyan20@gmail.com>
2023-07-14 19:15:03 +0300

    print all Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - this the main function
+* Return: is always 0
+*/
+int main(void)
+{
+
+	for (int i = 97; i <= 122; i++)
+	{
+		putchar((char)i);
+	}
+
+putchar('\n');
+return (0);
+}
