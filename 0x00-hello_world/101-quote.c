84f6aca85e4ed0a0e5ea95d1b49cf9995e9a1989
MoamenZyan <moamenzyan20@gmail.com>
2023-07-14 11:52:37 +0300

    return 1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+* main - this is the main function
+* Return: in this function only i returned 1
+*/
 int main(void)
 {
 	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
