209ddc8cd2e3929cce59400ac00f373e3e3e164b
MoamenZyan <moamenzyan20@gmail.com>
2023-07-15 18:25:11 +0300

    advanced #3

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+* main - this is the main function
+* Return: is always 0
+*/
+int main(void)
+{
+int i, j, k, l;
+for (i = 0; i <= 9; i++)
+{
+	for (j = 0; j <= 9; j++)
+	{
+		for (k = 0; k <= 9; k++)
+		{
+			for (l = 0; l <= 9; l++)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(' ');
+				putchar(k + '0');
+				putchar(l + '0');
+				if ((i == 9) && (j == 8) && (k == 9) && (l == 9))
+					goto endloop;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+
+	}
+}
+endloop:
+putchar('\n');
+return (0);
+}
