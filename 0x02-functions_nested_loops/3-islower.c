1adbf08b006c1200e1d8ab916a3b2d8bca3bd0bf
MoamenZyan <moamenzyan20@gmail.com>
2023-07-18 22:48:55 +0300

    is lower ?

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _islower - this is the  function
+* @c: takes input from other functions
+* Return: 0 if upper and 1 if lower
+*/
+int _islower(int c)
+{
+	if (c > 90)
+
+		return (1);
+	else
+		return (0);
+}
