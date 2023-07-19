e1030f1eb3725ad5057fd155c66d9ae50c75cad8
MoamenZyan <moamenzyan20@gmail.com>
2023-07-19 04:57:12 +0300

    add two number

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* add - function that adds two numbers
+* @x: this is input 1
+* @y: this is input 2
+* Return: return sum of the two numbers
+*/
+
+int add(int x, int y)
+{
+	return (x + y);
+}
+
