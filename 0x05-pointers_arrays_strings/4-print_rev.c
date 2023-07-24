6d63b94f99f30ccd822a8c19c006e284e725c1e7
MoamenZyan <moamenzyan20@gmail.com>
2023-07-25 02:53:19 +0300

    print reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* print_rev - this is the main function
+* Return: nothing
+* @s: input
+*/
+
+void print_rev(char *s)
+{
+
+int i;
+
+	for (i = (int)strlen(s); i >= -1; i--)
+	{
+		if (i == -1)
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(s[i]);
+	}
+
+}
