6905fcfdc50b174e18cfe792ee91ff307c0a446c
MoamenZyan <moamenzyan20@gmail.com>
2023-07-28 03:52:42 +0300

    cap_string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,70 @@
+#include "main.h"
+#include <stdio.h>
+
+/** 
+* isLower - determains wethere ascii is lower case
+* @c: char
+* Return: 1 or 0
+*/
+
+int isLower(char c)
+{
+
+	return (c >= 97 && c <= 122);
+
+}
+/**
+* isDelimiter - determains wethere ascii is lower case
+* @c: char
+* Return: 1 or 0
+*/
+
+int isDelimiter(char c)
+{
+
+int i;
+char delimiter[] = " \t\n,.!?\"(){}";
+
+for (i = 0; i < 12; i++)
+	if (c == delimiter[i])
+		return (1);
+return (0);
+
+}
+
+/**
+* *cap_string - determains wethere ascii is lower case
+* @s: char
+* Return: 1 or 0
+*/
+
+char *cap_string(char *s)
+{
+char *ptr = s;
+
+int foundDelimit = 1;
+
+while (*s)
+{
+	if (isDelimiter(*s))
+		foundDelimit = 1;
+	else if (isLower(*s) && foundDelimit)
+	{
+		*s -= 32;
+
+		foundDelimit = 0;
+	}
+	else
+		foundelimit = 0;
+	s++;
+}
+	return (ptr);
+
+}
+
+
+}
+
+}
+
+
