1bb9efe3ae13a9a631ddd219a931eef93ef7d206
MoamenZyan <moamenzyan20@gmail.com>
2023-07-31 21:58:30 +0300

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* *_strchr - this is the main function
+* Return: char s
+* @s: input1
+* @c: input2
+*/
+char *_strchr(char *s, char c)
+{
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+
+return (NULL);
+}
+
