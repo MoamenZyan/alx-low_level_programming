4343e06c5cddd386e377f6cab8c8636504e9a397
MoamenZyan <moamenzyan20@gmail.com>
2023-08-15 01:51:39 +0300

    1-init_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,12 +7,13 @@
 * @age: Second member
 * @owner: Third member
 */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
