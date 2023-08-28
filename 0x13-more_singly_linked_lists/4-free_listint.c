9f7a0ec846258f797978c06a8ba405e7ed4c93be
MoamenZyan <moamenzyan20@gmail.com>
2023-08-28 19:44:29 +0300

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint - frees all allocated memory space
+ * @head: first node of the list
+ * Return: void
+*/
+
+void free_listint(listint_t *head)
+{
+	while (head != NULL)
+	{
+		free(head);
+		head = head->next;
+	}
+}
+
