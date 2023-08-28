5c2b428bf90a97c8d31ba3157e7de50cf59da730
MoamenZyan <moamenzyan20@gmail.com>
2023-08-28 20:28:46 +0300

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,19 +1,25 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
 
+/**
+ * get_nodeint_at_index - get the the chosen node
+ * @head: first node in the list
+ * @index: index to choose the node
+ * Return: the chosen node
+*/
 
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
-	i = 0;
 
+	i = 0;
 	while (i != index && head != NULL)
 	{
 		i++;
 		head = head->next;
 	}
+
 	if (i == index)
 		return (head);
 	else
