1b77485f400d2b8bbfec9155d48f7c81c69629e0
Moamen Zyan <moamenzyan20@gmail.com>
2023-12-07 18:24:09 +0200

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -28,7 +28,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (current == NULL)
 		return (NULL);
 
-
 	(current->prev)->next = new;
 	new->prev = current->prev;
 	new->next = current;
