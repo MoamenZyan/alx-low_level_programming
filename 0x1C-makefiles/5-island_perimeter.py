6d2e69bdc434355c9583bb46943474294a0273bc
MoamenZyan <moamenzyan20@gmail.com>
2024-02-21 14:35:25 +0200

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
 
-
 """island_primeter function."""
 
 
 def island_perimeter(grid):
     """Grid Function that calculate perimeter of a grid."""
+    if (len(grid) == 0):
+        return 0
     horizontal = 0
     vertical = 0
     max = 0
-    for r in range(len(grid)):
+    for r in range(0, len(grid)):
         horizontal = 0
-        for c in range(len(grid)):
-            if (grid[r][c] == 1):
-                horizontal += 1
         if (1 in grid[r]):
             vertical += 1
-        if (max < horizontal):
+        for c in range(0, len(grid[r])):
+            if (grid[r][c] == 1):
+                horizontal += 1
+        if (max <= horizontal):
             max = horizontal
     return 2 * (max + vertical)
